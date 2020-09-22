52302ca19adefde8c5aa95dceef7b29b31e260a4
kvn <kevin.roualec@hotmail.fr>
2020-09-22 14:12:40 +0200

    Bug fixing

--- generateTrombi.js
+++ generateTrombi.js
@@ -15,7 +15,7 @@ function generateDoc() {
       var students = responseStudent.students;
       if(students && students.length > 0){
         var doc = DocumentApp.create("Trombinoscope " + course.name)
-        var fileId = '1_YCxlhUtOaIx5HmPUIlg_SjolbugXazl'; //penser à changer l'id du fichier image stocké sur le drive pour avoir le logo de l'établissement
+        var fileId = '1_YCxlhUtOaIx5HmPUIlg_SjolbugXazl'; //penser à changer l'id du fichier image stocké sur le drive pour avoir le logo
         var img = DriveApp.getFileById(fileId).getBlob();
         var img = doc.getChild(0).asParagraph().appendInlineImage(img)
         img.setWidth(275)
@@ -46,7 +46,9 @@ function generateDoc() {
                 if (x <4){
                    var td = tr.appendTableCell(studentName);
                    var resp = UrlFetchApp.fetch(photoUrl);
-                   table.getCell(y, x).insertImage(0, resp);
+                   var profilePic = table.getCell(y, x).insertImage(0, resp);
+                   profilePic.setWidth(90)
+                   profilePic.setHeight(120)
                    x++;
                    //Impossible de centrer le nom des étudiants, GAP ne permets pas le centrage dans les cells d'une table (https://issuetracker.google.com/issues/36765133)
                    }
@@ -57,16 +59,17 @@ function generateDoc() {
                   }
              }    
          }
-      }   
-    doc.saveAndClose()    
+      doc.saveAndClose()    
     var docId = doc.getId();
     var docFolder = DriveApp.getFileById(docId).getParents().next().getId();
     var docblob = doc.getAs('application/pdf');
     docblob.setName(doc.getName() + ".pdf");
     var file = DriveApp.createFile(docblob);
-    var fileId = file.getId();      
-    Logger.log("pdf done")
-   }
+    var fileId = file.getId();
+    var fileURL = file.getUrl(); 
+    Logger.log("pdf done "+ fileURL)
+    }   
+    }
   }
   else {
   Logger.log(course.name, course.id, '=> Erreur: aucun etudiant dans cette classe. Trombinoscope abandonné !');
